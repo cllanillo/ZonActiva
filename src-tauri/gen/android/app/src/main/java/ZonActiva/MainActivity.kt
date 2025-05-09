12f5c315295f4f21e2db73ac5184943e56b7380c
@cllanillo <cllanillo@soincon.es>
2025-05-09 03:36:32 +0200

    build: 🎇 android init!!!

---
+++ src-tauri/gen/android/app/src/main/java/ZonActiva/MainActivity.kt
@@ -0,0 +1,3 @@
+package ZonActiva
+
+class MainActivity : TauriActivity()
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { pigment, type PigmentOptions } from '@pigment-css/vite-plugin';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, type HmrOptions } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 import { theme } from './src/configuration/theme';
@@ -9,11 +9,28 @@ import { theme } from './src/configuration/theme';
 const pigmentConfig: PigmentOptions = {
     transformLibraries: [
         '@mui/material',
+        // '@mui/x-date-pickers-pro'
     ],
+    // exclude:[/dayjs.*/i],
     theme,
 };
 
+const tauriHmr: HmrOptions = { protocol: 'ws', host: process.env.TAURI_DEV_HOST, port: 3000 - 1 };
+
 export default defineConfig({
+    build: {
+        target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+        ...(!!process.env.TAURI_ENV_DEBUG && { minify: false, sourcemap: true })
+    },
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_ENV_'],
+    server: {
+        port: 3000,
+        strictPort: true,
+        host: tauriHmr.host || false,
+        hmr: tauriHmr.host ? tauriHmr : undefined,
+        watch: { ignored: ['**/src-tauri/**'] }
+    },
     plugins: [
         pigment(pigmentConfig),
         TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
