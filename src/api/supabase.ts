281b3ac74fd6ac134abc9686e53eba3d52756b9d
@cllanillo <cllanillo@soincon.es>
2025-06-11 23:41:53 +0200

    fix: update Supabase client configuration with hardcoded URL and key

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://tlerhfbhwgegtdhbhtlf.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsZXJoZmJod2dlZ3RkaGJodGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTc4NTQsImV4cCI6MjA2MTY3Mzg1NH0.407jBA_JUJIp4eq97iRLSruVUPdh4vHucWDsDnzFl_8";
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
